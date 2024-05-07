ec86fb86186c1723eda51ae5890288273f97bf69
theo-baccam <theo.baccam@laplateforme.io>
2024-05-07 13:38:50 +0200

    feat(gol_algorithm): added header file for functions and variables

---
+++ include/gol_algorithm.hpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+
+struct Cell {
+    bool currentState;
+    bool nextState;
+}
+typedef Cell Cell;
+
+std::vector<std::vector<Cell>> matrix;
+
+getNeighborsCount(int y, int x);
+willBeBorn(bool cellCurrentState, int neighborsCount);
+willDie(int neighborsCount);
+setNewStates(std::vector<std::vector<Cell>>& matrix);
+
+setCurrentStates(std::vector<std::vector<Cell>>& matrix);
