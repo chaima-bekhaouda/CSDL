6841b8056f544d91ec23aa8382d27b82d2aede87
theo-baccam <theo.baccam@laplateforme.io>
2024-05-07 16:27:46 +0200

    feat: basic file write/read

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,44 @@
 #include <iostream>
 #include <vector>
+#include <fstream> // TEMPORARY !!!!
 #include "gol_algorithm.hpp"
 
 
 int main() {
+    /*
     std::vector<std::vector<Cell>> matrix({
-        {{0, 0}, {0, 0}, {1, 0}},
-        {{1, 0}, {0, 0}, {1, 0}},
-        {{0, 0}, {1, 0}, {1, 0}}
+        {{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}},
+        {{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}},
+        {{0, 0}, {0, 0}, {1, 0}, {1, 0}, {1, 0}, {0, 0}, {0, 0}},
+        {{0, 0}, {0, 0}, {1, 0}, {0, 0}, {1, 0}, {0, 0}, {0, 0}},
+        {{0, 0}, {0, 0}, {1, 0}, {0, 0}, {1, 0}, {0, 0}, {0, 0}},
+        {{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}},
+        {{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}}
     });
+    std::string textFileContent = "!default.cells\n";
     for (int y = 0; y < matrix.size(); y++) {
         for (int x = 0; x < matrix.size(); x++) {
-            std::cout << matrix[y][x].currentState << " ";
+            switch (matrix[y][x].currentState) {
+                case 0: textFileContent += '.'; break;
+                case 1: textFileContent += 'O'; break;
+            };
         };
-        std::cout << std::endl;
+        textFileContent += '\n';
     };
-    std::cout << getNeighborsCount(1, 1, matrix) << std::endl;
+    std::cout << textFileContent << std::endl;
+
+    std::ofstream file("default.cells");
+    file << textFileContent;
+    file.close();
+    */
+    std::string line;
+    std::ifstream file("default.cells");
+    while (getline (file, line)) {
+        if (line[0] == '!') {
+            continue;
+        }
+        std::cout << line << ' ' << line.length() << '\n';
+    };
+    file.close();
     return 0;
 }
