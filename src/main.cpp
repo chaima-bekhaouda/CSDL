8f216f0d503bcd88d25edab06679f35fe60daa90
theo-baccam <theo.baccam@laplateforme.io>
2024-05-07 15:21:40 +0200

    feat: temporary text display of matrix

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,16 @@
 
 
 int main() {
+    matrix = std::vector<std::vector<Cell>> {
+        {{0, 0}, {0, 0}, {1, 0}},
+        {{1, 0}, {0, 0}, {1, 0}},
+        {{0, 0}, {1, 0}, {1, 0}}
+    };
+    for (int y = 0; y < matrix.size(); y++) {
+        for (int x = 0; x < matrix.size(); x++) {
+            std::cout << matrix[y][x].currentState << " ";
+        };
+        std::cout << std::endl;
+    };
     return 0;
 }
