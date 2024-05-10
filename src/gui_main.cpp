bf808ce43a2c8cf3ba6b8a53c19d72f2d2c7e608
theo-baccam <theo.baccam@laplateforme.io>
2024-05-10 13:28:51 +0200

    feat(gui): added menu button

--- src/gui_main.cpp
+++ src/gui_main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "raylib.h"
 #include "gui_colors.hpp"
 
@@ -5,11 +6,42 @@
 int main() {
     InitWindow(1024, 720, "The Game Of Life");
 
+    bool menuUp = false;
+    Texture menuButton = LoadTexture("resources/menu-button.png");
+    Rectangle menuButtonSource = {
+        0,
+        0,
+        (float)menuButton.width,
+        (float)menuButton.height
+    };
+    Rectangle menuButtonBounds = {
+        -64,
+        35,
+        (float)menuButton.width, 
+        (float)menuButton.height
+    };
+
     while (!WindowShouldClose()) {
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            if (CheckCollisionPointRec(GetMousePosition(), menuButtonBounds)) {
+                menuUp ? menuUp = false : menuUp = true;
+                std::cout << menuUp << '\n';
+            };
+        };
+
         BeginDrawing();
             ClearBackground(DEEPOCEANBLUE);
+
+            DrawTextureRec(
+                menuButton,
+                menuButtonSource,
+                (Vector2){menuButtonBounds.x, menuButtonBounds.y},
+                WHITE
+            );
         EndDrawing();
-    }
+    };
+
+    CloseWindow();
 
     return 0;
 }
